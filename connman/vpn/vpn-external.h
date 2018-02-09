53b9410c5e41460e43c2526c940e94e086d6a33d
Jussi Laakkonen <jussi.laakkonen@jolla.com>
2018-02-09 17:16:52 +0200

    [connman] Add a "vpn_external.h" header to be used for external VPN plugin development. Contributes to JB#39969
    Add VPN headers to be included in devel package for external VPN plugins.

---
+++ connman/vpn/vpn-external.h
@@ -0,0 +1,36 @@
+/*
+ *
+ *  ConnMan VPN daemon external plugin development header
+ *
+ *  Copyright (C) 2018 Jolla LTD.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef __CONNMAN_VPND_VPN_EXTERNAL_H
+#define __CONNMAN_VPND_VPN_EXTERNAL_H
+
+#include "plugins/vpn.h" /* Includes vpn-provider.h */
+#include "vpn-agent.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __CONNMAN_VPND_VPN_EXTERNAL_H */
