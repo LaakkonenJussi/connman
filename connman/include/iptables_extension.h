07c5e4bcf63cb37587fc40d1c06fb39444acc272
Jussi Laakkonen <jussi.laakkonen@jolla.com>
2018-02-20 17:46:30 +0200

    [connman] Sailfish iptables API support. Contributes to JB#39338
    Adds support for managing iptables content via ConnMan. The header to
    include: iptables_extension.h, which is installed as part of devel package.
    
    Functionalities implemented for plugins to use:
     - Chain management (new, delete, flush, find)
     - Iptables rule management (insert, append, delete)
     - Commit iptables changes
     - Change iptables policy
     - Save, load and clear iptables
        - default file STORAREDIR/iptables/rules.v4 to store rules
        - Saving of iptables feature is adapted from iptables source.
     - Get iptables content
    
    These functionalities do not restrict use of specific iptables table.
    Saving and loading should be requested by the external plugin.
    
    Upgrade iptables' xtables library build requirement to >= 1.6.1.
    
    Upgrade iptables install requirement to >= 1.6.1.

---
+++ connman/include/iptables_extension.h
@@ -0,0 +1,92 @@
+/*
+ *
+ *  Connection Manager wrapper to expose iptables functions for SailfishOS MDM.
+ *
+ *  Copyright (C) 2017 Jolla Ltd. All rights reserved.
+ *  Contact: Jussi Laakkonen <jussi.laakkonen@jolla.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef _CONNMAN_IPTABLES_EXTENSION_H
+#define _CONNMAN_IPTABLES_EXTENSION_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct iptables_content {
+	gchar *table;
+	GList *chains;
+	GList *rules;
+};
+
+typedef struct iptables_content connman_iptables_content;
+
+/* IPTABLES FUNCTIONS */
+
+int connman_iptables_new_chain(const char *table_name,
+					const char *chain);
+	
+int connman_iptables_delete_chain(const char *table_name,
+					const char *chain);
+
+int connman_iptables_flush_chain(const char *table_name,
+					const char *chain);
+
+int connman_iptables_find_chain(const char *table_name,
+					const char *chain);
+
+int connman_iptables_insert(const char *table_name,
+				const char *chain,
+				const char *rule_spec);
+
+int connman_iptables_append(const char *table_name,
+				const char *chain,
+				const char *rule_spec);
+	
+int connman_iptables_delete(const char *table_name,
+				const char *chain,
+				const char *rule_spec);
+
+int connman_iptables_commit(const char *table_name);
+
+int connman_iptables_change_policy(const char *table_name,
+					const char *chain,
+					const char *policy);
+					
+int connman_iptables_save(const char* tablename, const char *fpath);
+
+int connman_iptables_restore(const char* tablename, const char *fpath);
+
+int connman_iptables_clear(const char* tablename);
+
+const char* connman_iptables_default_save_path(int ip_version);
+
+connman_iptables_content* connman_iptables_get_content(const char *table_name);
+void connman_iptables_free_content(connman_iptables_content *content);
+
+					
+/* IPTABLES FUNCTIONS END */
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _CONNMAN_IPTABLES_EXTENSION_H */
